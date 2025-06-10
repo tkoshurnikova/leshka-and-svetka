768ba72543690149f3823776b9a567295384613c
tkosh <taniaraduga@gmail.com>
2025-06-10 20:35:34 +0500

    modal

--- js/script.js
+++ js/script.js
@@ -17,3 +17,49 @@ async function copyToClipboard(text) {
         }, 1000);
     }
 }
+
+const form = document.querySelector('.js-form');
+const modal = document.querySelector('.js-modal');
+const overlay = document.querySelector('.overlay');
+
+async function onFormSubmit(e) {
+    try {
+        const formData = new FormData(form);
+
+        const response = await fetch('https://formspree.io/f/mpwrprew', {
+            method: 'post',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            showSuccess();
+        } else {
+            showError();
+        }
+    } catch (err) {
+        console.log('Произошла следующая ошибка:', err);
+        showError();
+    }
+}
+
+overlay.addEventListener('click', closeModal);
+
+function showSuccess() {
+    modal.classList.add('_success');
+    overlay.style.display = 'block';
+    form.reset();
+}
+
+function showError() {
+    modal.classList.add('_error');
+    overlay.style.display = 'block';
+}
+
+function closeModal() {
+    modal.classList.remove('_success');
+    modal.classList.remove('_error');
+    overlay.style.display = 'none';
+}
