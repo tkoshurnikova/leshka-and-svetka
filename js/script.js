b6e38b40a749e9cbd5d419301ba5882766166934
tkosh <taniaraduga@gmail.com>
2025-06-10 17:27:43 +0500

    fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 async function copyToClipboard(text) {
+    const informer  = document.querySelector('.js-informer');
+
     try {
         await navigator.clipboard.writeText(text);
-        console.log('Текст успешно скопирован в буфер обмена!');
-        return true;
+
+        informer.style.opacity = 1;
+        setTimeout(() => {
+            informer.style.opacity = 0;
+        }, 1000);
     } catch (err) {
         console.error('Ошибка при копировании текста в буфер обмена:', err);
-        return false;
+        informer.innerHTML = 'Возникла ошибка, обновите страницу';
+        informer.style.opacity = 1;
+        setTimeout(() => {
+            informer.style.opacity = 0;
+        }, 1000);
     }
 }
